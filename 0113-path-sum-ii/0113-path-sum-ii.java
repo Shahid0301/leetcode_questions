f806bc45e2547223cd0667d0356faca1c7fe8026
Shahid khan <86796608+Shahid0301@users.noreply.github.com>
2023-01-12 15:21:27 +0530

    Time: 72 ms (5.35%), Space: 43.2 MB (60.06%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.java
+++ 0113-path-sum-ii/0113-path-sum-ii.java
@@ -1,26 +1,34 @@
-class Solution {
-    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
-        List<Integer> currentPath = new ArrayList();
-        List<List<Integer>> allPaths = new ArrayList();
-        findPathsRecursive(root,targetSum,currentPath,allPaths);
-        return allPaths;
-    }
-    private static void findPathsRecursive(TreeNode currentNode, int sum, List<Integer> currentPath,List<List<Integer>> allPaths){
-         if(currentNode==null)return;
-         // add the current node to the path
-         currentPath.add(currentNode.val);
-         // if the current node is a leaf and its value is equal to sum, save the current path
-         if(currentNode.val==sum && currentNode.left==null && currentNode.right==null)
-             allPaths.add(new ArrayList<Integer>(currentPath));
-         else{
-             // traverse the left sub-tree
-             findPathsRecursive(currentNode.left,sum-currentNode.val,currentPath,allPaths);
-             // traverse the right sub-tree
-             findPathsRecursive(currentNode.right,sum-currentNode.val,currentPath,allPaths);
-         }
-         // remove the current node from the path to backtrack, 
-         // we need to remove the current node while we are going up the recursive call stack.
-         currentPath.remove(currentPath.size()-1);
-      }
 
+class Solution {
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum)
+    {
+        List<List<Integer>> list=new ArrayList<>();
+        List<Integer> l=new ArrayList<>();
+        findSum(root,targetSum,list,l,0);
+        return list;
+    }
+    public void findSum(TreeNode root,int t,List<List<Integer>> list,List<Integer> l,int  sum)
+    {
+        // if(sum>t)return;
+        if(root==null)return;
+        if(root.left==null && root.right==null)
+        {
+            sum+=root.val;
+            if(sum==t) 
+            {
+                System.out.println("Yes");
+                l.add(root.val);
+                list.add(new ArrayList<>(l));
+                l.remove(l.size()-1);
+            }
+            for(int i: l)
+               System.out.print(i+" ");  
+            System.out.println();
+            return;
+        }
+        l.add(root.val);
+        findSum(root.left,t,list,l,sum+root.val);
+        findSum(root.right,t,list,l,sum+root.val);
+        l.remove(l.size()-1);
+    }
 }
