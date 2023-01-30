d2eb8b929ebe462d4edac3f8a8f55f304bc9af84
Shahid khan <86796608+Shahid0301@users.noreply.github.com>
2023-01-30 16:15:56 +0530

    Time: 1 ms (100.00%), Space: 39.2 MB (97.85%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        int count=0;
+        while(n!=0){
+            if(n%2==1){
+count++;}
+            n/=2;
+        }
+        return count==1?true:false;
+    }
+}
