a82743ab115fe137639137e57ef3be1dcba6245b
Shahid khan <86796608+Shahid0301@users.noreply.github.com>
2023-03-14 01:13:58 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (48.08%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,36 +1,25 @@
 class Solution {
-    public int search(int[] nums, int target) {
-        int ind=-1;
-        int index=-1;
-        for(int i=0;i<nums.length-1;i++){
-            if(nums[i]>nums[i+1]){
-                ind=i;
+    public int search(int[] arr, int target) {
+        int low = 0, high = arr.length - 1; //<---step 1
+
+        while (low <= high) { //<--- step 2
+            int mid = (low + high) >> 1; //<----step 3
+            if (arr[mid] == target)
+                return mid; // <---step 4
+
+            if (arr[low] <= arr[mid]) { //<---step 5
+                if (arr[low] <= target && arr[mid] >= target)
+                    high = mid - 1; //<---step 6 
+                else
+                    low = mid + 1; //<---step 7
+            } else { //<---step 7
+                if (arr[mid] <= target && target <= arr[high])
+                    low = mid + 1;
+                else
+                    high = mid - 1;
             }
         }
-        
-        index=search(0,ind,target,nums);
-        if(index==-1){
-            index=search(ind+1,nums.length-1,target,nums);
-            
-        }
-        
-        return index;
-    }
-        
-    public int search(int si,int ei,int m,int[] arr){
-			while(si<=ei){
-				int mid=(si+ei)/2;
-				if(arr[mid]==m){
-					return mid;
-				}
-				if(arr[mid]>m){
-					ei=mid-1;
-				}
-				if(arr[mid]<m){
-					si=mid+1;
-				}
-			}
-			return -1;
+        return -1; //<---step 8
     }
         
 }
