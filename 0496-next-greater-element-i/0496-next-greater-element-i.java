29546a14518fb3c9a0dfc9810be5a0a9b9d11d36
Shahid khan <86796608+Shahid0301@users.noreply.github.com>
2023-04-19 23:08:27 +0530

    Time: 6 ms (27.86%), Space: 42.5 MB (83.66%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,8 +1,8 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        Stack<Integer> st=new Stack<>();
         int arr[]=new int[nums1.length];
         int flag=0;
+        int ans=0;
         for(int i=0;i<nums1.length;i++){
             for(int j=0;j<nums2.length;j++){
                 if(nums2[j]==nums1[i]){
@@ -10,19 +10,23 @@ class Solution {
                 }
                 if(flag==1){
                     if(nums2[j]>nums1[i]){
-                        st.push(nums2[j]);
+                        arr[i]=nums2[j];
+                        ans=1;
                         break;
-                }
-                else{
+                    }
+                    else
+                    {
                     continue;
-                }
+                        }
                 }
             }
+             if(ans==0){
+                 arr[i]=-1;
+             }
             flag=0;
-            if(!st.isEmpty())
-                 arr[i]=st.pop();
-            else
-                arr[i]=-1;
+           
+            ans=0;
+
         }
         return arr;
     }
