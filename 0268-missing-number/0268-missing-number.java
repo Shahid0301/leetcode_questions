7dd3afdd2030e4b7c9efd2029889b682423634de
Shahid khan <86796608+Shahid0301@users.noreply.github.com>
2024-07-24 23:35:19 +0530

    Time: 0 ms (100%), Space: 45.7 MB (5.82%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,13 +1,19 @@
 class Solution {
-    public int missingNumber(int[] nums) {
-        Arrays.sort(nums);
-        int i=0;
-        while(i<nums.length){
-            if(nums[i]!=i){
-                return i;
-            }
-            i++;
+    public int missingNumber(int[] a) {
+        int xor1 = 0, xor2 = 0;
+        int N = a.length; // Length of the array
+
+        // Calculate xor2 as the XOR of all array elements
+        // Calculate xor1 as the XOR of numbers from 0 to N
+        for (int i = 0; i < N; i++) {
+            xor2 = xor2 ^ a[i];
+            xor1 = xor1 ^ i;
         }
-    return i;
-}
+
+        // Include the last number N in xor1
+        xor1 = xor1 ^ N;
+
+        // The missing number will be the XOR of xor1 and xor2
+        return (xor1 ^ xor2);
+    }
 }
